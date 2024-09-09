3a08cc3185a2d338491cada2ad809d30790f326b
P1su <pissssssu@naver.com>
2024-09-09 21:24:23 +0900

    feat: Outlet을 통한 중첩 라우팅 구현

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Start from '../pages/Start/Start';
+import Layout from '../layouts/Layout';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Layout />
     children: [
       {
         path: '/',
