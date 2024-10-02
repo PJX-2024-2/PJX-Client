d1b40d720a4ba73d68e62c77ecb0d7af71662c59
Wang HoEun <hoeunwang@gmail.com>
2024-10-02 15:53:21 +0900

    feat: 캘린더 상단 요일 표시 부분 구현

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,1 +1,2 @@
 export { default as IcPen } from './IcPen';
+export { default as IcSearch } from './IcSearch';
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { IcSearch } from '../../assets/svg';
 import MonthlyCost from '../../components/MonthlyCost/MonthlyCost';
 import * as S from './Home.style';
 import React from 'react';
@@ -8,6 +9,13 @@ const Home = () => {
     return (
         <S.HomeWrapper>
             <MonthlyCost/>
+            <S.DateWrapper>
+            <S.YearMonthDate>2024년 09월 03일</S.YearMonthDate>
+            <S.IconWrapper>
+            <S.Day>목요일</S.Day>
+            <IcSearch/>
+            </S.IconWrapper>
+            </S.DateWrapper>
         </S.HomeWrapper>
     );
 };
