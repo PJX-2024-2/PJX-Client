9aa8e356c94d8b1c3081047be95725292b1ef1ce
P1su <pissssssu@naver.com>
2024-10-03 01:03:55 +0900

    feat: Step1 이메일 입력 컴포넌트 생성

--- src/components/OnboardingSteps/Step1/Step1.style.ts
+++ src/components/OnboardingSteps/Step1/Step1.style.ts
@@ -6,7 +6,10 @@ export const Step1Wrapper = styled.div`
   width: 100%;
   height: 100vh;
   padding: 6.4rem 1.6rem 0 1.6rem;
-  gap: 4.4rem;
+`;
+export const LayoutBox = styled.div`
+  width: 100%;
+  height: 30rem;
 `;
 export const InputBox = styled.div`
   display: flex;
@@ -15,21 +18,23 @@ export const InputBox = styled.div`
   border-radius: 10px;
   border: solid 1.2px ${({ theme }) => theme.colors.gray[200]};
   padding: 1.5rem 1.6rem 1.5rem 1.6rem;
+  margin-top: 4.4rem;
   gap: 1rem;
 `;
 export const EmailInput = styled.input`
   ${({ theme }) => theme.fonts.r_16_400};
   border: none;
-  width: 25.5rem;
+  width: 20.5rem;
   height: 2.2rem;
 `;
 export const DomainCheckSpan = styled.span`
+  ${({ theme: { mixin }}) => mixin.flexBox({ justify: 'flex-end' })};
   ${({ theme }) => theme.fonts.r_16_500};
   color: ${({ theme }) => theme.colors.primary.default};
-  width: 5rem;
+  width: 9rem;
+  cursor: pointer;
 `;
 export const ButtonField = styled.div`
   ${({ theme: { mixin }}) => mixin.flexBox({ justify: 'flex-end' })};
   width: 100%;
-  padding-top: 22.5rem;
 `;
--- src/components/OnboardingSteps/Step1/Step1.tsx
+++ src/components/OnboardingSteps/Step1/Step1.tsx
@@ -1,19 +1,27 @@
 import BtnSmall from '../../common/Button/SmallButton/BtnSmall';
+import Dropbox from '../../common/Dropbox/Dropbox';
 import OnboardingTitle from '../OnboardingTitle/OnboardingTitle';
 import * as S from './Step1.style';
 import React, { ChangeEvent, useState } from 'react';
 
 function Step1 (){
   const [value, setValue] = useState<string>('');
+  const [domainValue, setDomainValue] = useState<string>('@선택');
+  const [isClick, setIsClick] = useState<boolean>(false);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
-  const handleDropbox = () => {
-    alert('드롭박스 내려옴......');
+  const handleClick = () => {
+    setIsClick(!isClick);
+  };
+  const handleDropbox = (newDomain: string) => {
+    setIsClick(!isClick);
+    setDomainValue(newDomain);
   };
   const handleNext = () => {
-    //이메일 저장 및 다음 단계로 이동
+    const email = value + domainValue;
+    console.log(email);
     alert('이동합니다');
   };
 
@@ -24,15 +32,22 @@ function Step1 (){
         본인 이메일이 맞다면 <br />
         아래 확인 버튼을 눌러주세요
       </OnboardingTitle>
-      <S.InputBox>
-        <S.EmailInput 
-          placeholder='이메일'
-          name={value}
-          value={value}
-          onChange={onChange}
-        />
-        <S.DomainCheckSpan onClick={() => handleDropbox()}>@선택</S.DomainCheckSpan>
-      </S.InputBox>
+      <S.LayoutBox>
+        <S.InputBox>
+          <S.EmailInput 
+            placeholder='이메일'
+            name={value}
+            value={value}
+            onChange={onChange}
+          />
+          <S.DomainCheckSpan onClick={() => handleClick()}>{domainValue}</S.DomainCheckSpan>
+        </S.InputBox>
+        {
+          isClick && (
+            <Dropbox onClick={handleDropbox} />
+          )
+        }
+      </S.LayoutBox>
       <S.ButtonField>
         <BtnSmall onClick={handleNext}>확인</BtnSmall>
       </S.ButtonField>
