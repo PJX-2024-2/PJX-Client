340d54335f82c3b3a26f545e2960c61d9acce4e5
P1su <pissssssu@naver.com>
2024-10-02 12:24:28 +0900

    style: 버튼 컴포넌트 색상을 변경된 theme 객체값으로 적용

--- src/components/common/Button/SmallButton/BtnSmall.style.ts
+++ src/components/common/Button/SmallButton/BtnSmall.style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ButtonWrapper = styled.button`
   ${({ theme: { mixin } }) => mixin.flexCenter({})};
   ${({ theme }) => theme.fonts.b_20_600};
-  background-color: ${({ theme }) => theme.colors.orange.O_100};
+  background-color: ${({ theme }) => theme.colors.orange.primary_default};
   color: ${({ theme }) => theme.colors.normal.white};
   width: 10.4rem;
   height: 5.6rem;
