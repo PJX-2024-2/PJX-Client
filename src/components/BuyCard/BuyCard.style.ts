a31811ba8c2e71d7238887569db6f3da36eb5ed2
Wang HoEun <hoeunwang@gmail.com>
2024-10-08 16:30:13 +0900

    feat: 사진 wrapper 생성 후 2가지 들어가게끔 해둠

--- src/components/BuyCard/BuyCard.style.ts
+++ src/components/BuyCard/BuyCard.style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const BuyCardWrapper = styled.div`
 ${({ theme: { mixin } }) => mixin.flexBox({direction: 'column',align: 'flex-start'})};
-width: 100%;
+width: 34.3rem;
 padding: 0 0.4rem;
 gap: 1rem;
 `;
@@ -10,6 +10,7 @@ gap: 1rem;
 export const BuyName = styled.div`
 ${({ theme: { mixin } }) => mixin.flexBox({justify: 'space-between', align: 'center'})};
 width: 100%;
+
 p{
 ${({theme})=> theme.fonts.m_13_600};
 color: ${({theme})=> theme.colors.gray[700]};
@@ -35,10 +36,21 @@ border: 1px solid rgba(0, 0, 0, 0.12);
 export const BuyText = styled.div`
 `;
 
-export const BuyPicture = styled.div`
+export const BuyPictureWrapper = styled.div`
+${({ theme: { mixin } }) => mixin.flexBox({justify: 'center'})};
+gap: 1rem;
+width: 100%;
 `;
 
+export const BuyPicture = styled.img`
+border-radius: 10px;
+border: 1px solid rgba(0, 0, 0, 0.03);
+width: 100%;
+height: 15rem;
+`;
+
+
 export const BuyContent = styled.div`
-${({ theme: { mixin } }) => mixin.flexBox({})};
+${({ theme: { mixin } }) => mixin.flexBox({direction: 'column'})};
 width: 100%;
 `;
--- src/components/BuyCard/BuyCard.tsx
+++ src/components/BuyCard/BuyCard.tsx
@@ -10,7 +10,10 @@ const BuyCard = () => {
             <S.BuyContent>
             <S.BuyName><p>바이바이 샐러드</p><S.CostEmozWrapper><S.BuyCost>42,000원</S.BuyCost> <EmotionSelect/></S.CostEmozWrapper></S.BuyName>
             <S.BuyText></S.BuyText>
-            <S.BuyPicture></S.BuyPicture>
+            <S.BuyPictureWrapper>
+                <S.BuyPicture src="https://github.com/user-attachments/assets/fee098da-2db0-459e-adcd-94b1364fd511"/>
+                <S.BuyPicture src="https://github.com/user-attachments/assets/fee098da-2db0-459e-adcd-94b1364fd511"/>
+            </S.BuyPictureWrapper>
             </S.BuyContent>
             <EmotionList/>
         </S.BuyCardWrapper>
