94fd550983af3a18ceaa493bbadfe8e8387c548b
Wang HoEun <hoeunwang@gmail.com>
2024-10-08 16:41:35 +0900

    refactor: BuyCard props 로 내용 넘길 수 있게끔 수정

--- src/components/BuyCard/BuyCard.tsx
+++ src/components/BuyCard/BuyCard.tsx
@@ -3,16 +3,23 @@ import * as S from './BuyCard.style';
 import EmotionList from "../EmotionList/EmotionList";
 import EmotionSelect from "../EmotionSelect/EmotionSelect";
 
+interface BuyCardProps {
+    name: string;
+    cost: string;
+    text: string;
+    img: string;
+}
 
-const BuyCard = () => {
+
+const BuyCard = ({name,cost,text,img}:BuyCardProps) => {
     return (
         <S.BuyCardWrapper>
             <S.BuyContent>
-            <S.BuyName><p>바이바이 샐러드</p><S.CostEmozWrapper><S.BuyCost>42,000원</S.BuyCost> <EmotionSelect/></S.CostEmozWrapper></S.BuyName>
-            <S.BuyText>집 근처에 있는 샐러드 가게에 갔는데 안사먹을 수가 없었다...진짜 대박 맛집이었어...애들아 다들 먹어....ㅋㅋㅋ</S.BuyText>
+            <S.BuyName><p>{name}</p><S.CostEmozWrapper><S.BuyCost>{cost}원</S.BuyCost> <EmotionSelect/></S.CostEmozWrapper></S.BuyName>
+            <S.BuyText>{text}</S.BuyText>
             <S.BuyPictureWrapper>
-                <S.BuyPicture src="https://github.com/user-attachments/assets/fee098da-2db0-459e-adcd-94b1364fd511"/>
-                <S.BuyPicture src="https://github.com/user-attachments/assets/fee098da-2db0-459e-adcd-94b1364fd511"/>
+                <S.BuyPicture src={img}/>
+                <S.BuyPicture src={img}/>
             </S.BuyPictureWrapper>
             </S.BuyContent>
             <EmotionList/>
--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -5,7 +5,15 @@ import BuyCard from '../../components/BuyCard/BuyCard';
 
 const Feed = () => {
     return (
-        <S.FeedWrapper> <Search/><BuyCard/></S.FeedWrapper>
+        <S.FeedWrapper> 
+            <Search/>
+            <BuyCard 
+                name="바이바이샐러드" 
+                cost="42,000" 
+                text="집 근처에 있는 샐러드 가게에 갔는데 안사먹을 수가 없었다...진짜 대박 맛집이었어...애들아 다들 먹어....ㅋㅋㅋ" 
+                img="https://github.com/user-attachments/assets/fee098da-2db0-459e-adcd-94b1364fd511"
+            />
+        </S.FeedWrapper>
     )
 }
 
