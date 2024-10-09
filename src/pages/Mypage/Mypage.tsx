adf22c02bc0cd2a1803eb0ee137db1a87a890086
P1su <pissssssu@naver.com>
2024-10-09 18:06:02 +0900

    feat: 반복되는 코드를 map으로 표현

--- src/pages/Mypage/Mypage.tsx
+++ src/pages/Mypage/Mypage.tsx
@@ -3,10 +3,41 @@ import * as S from './Mypage.style';
 import SmallTitle from '../../components/common/Title/SmallTitle/SmallTitle';
 import mockImage from '../../assets/profileMockImage.png';
 import { IcCheck, IcPen, IcRight, IcSearch } from '../../assets/svg';
+import { useNavigate } from 'react-router-dom';
 
 const Mypage = () => {
-  
+  const navigate = useNavigate();
+  const options = [
+    {
+      text: '내 지출 요약',
+      icon: <IcCheck fill='#000000' />,
+      onclick: () => {navigate('/')},
+    },
+    {
+      text: '내 또래 친구 목록',
+      icon: <IcSearch />,
+      onclick: () => {navigate('/friends')},
+    },
+  ];
 
+  const summaries = [
+    {
+      label: 'days',
+      value: '1034',
+      text: '일 동안',
+    },
+    {
+      label: 'costs',
+      value: '1000000',
+      text: '원을 아꼈어요',
+    },
+    {
+      label: 'friends',
+      value: '3',
+      text: '명의 또래 친구가 생겼어요',
+    },
+  ];
+  
   return (
     <S.MyPageWrapper>
       <SmallTitle>마이페이지</SmallTitle>
@@ -18,21 +49,24 @@ const Mypage = () => {
         </S.NicknameBox>
       </S.ProfileBox>
       <S.SummaryBox>
-        <p><span>1034</span>일 동안</p>
-        <p><span>100000</span>원을 아꼈어요</p>
-        <p><span>3</span>명의 또래 친구가 생겼어요</p>
+        {
+          summaries.map((summary) => (
+            <p key={summary.label}>
+              <span>{summary.value}</span>{summary.text}
+            </p>
+          ))
+        }
       </S.SummaryBox>
       <S.NavigateField>
-        <S.NavigateBox>
-          <IcCheck fill='#000000'/>
-          <p>내 지출 요약</p>
-          <IcRight />
-        </S.NavigateBox>
-        <S.NavigateBox>
-          <IcSearch />
-          <p>내 또래 친구 목록</p>
-          <IcRight />
-        </S.NavigateBox>
+        {
+          options.map((option) => (
+            <S.NavigateBox key={option.text} onClick={option.onclick}>
+              {option.icon}
+              <p>{option.text}</p>
+              <IcRight />
+            </S.NavigateBox>
+          ))
+        }
       </S.NavigateField>
     </S.MyPageWrapper>
   );
