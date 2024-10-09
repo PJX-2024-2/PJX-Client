065421f01e97538798dab919bba5e5092307b0c2
P1su <pissssssu@naver.com>
2024-10-09 20:16:32 +0900

    feat: 마이페이지 친구 목록 페이지 뷰 구현

--- src/components/common/Title/SmallTitle/SmallTitle.style.ts
+++ src/components/common/Title/SmallTitle/SmallTitle.style.ts
@@ -5,4 +5,5 @@ export const SmallTitleWrapper = styled.div`
   ${({ theme }) => theme.fonts.sb_22_600};
   letter-spacing: -0.22px;
   width: 100%;
+  gap: 1rem;
 `;
--- src/pages/Mypage/MypageFriends/MypageFriends.tsx
+++ src/pages/Mypage/MypageFriends/MypageFriends.tsx
@@ -1,10 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import SmallTitle from '../../../components/common/Title/SmallTitle/SmallTitle';
+import FriendList from '../../../components/Friend/FriendList/FriendList';
 import * as S from './MypageFriends.style';
 import React from 'react';
+import { IcBack } from '../../../assets/svg';
 
 const MypageFriends = () => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
+
   return(
     <S.MypageFriendsWrapper>
-      마이페이지 친구목록
+      <SmallTitle>
+        <S.IconBox onClick={() => handleBack()}>
+          <IcBack />
+        </S.IconBox>
+        내 또래 친구 목록
+      </SmallTitle>
+      <S.FriendListBox>
+        <FriendList />
+      </S.FriendListBox>
     </S.MypageFriendsWrapper>
   );
 };
