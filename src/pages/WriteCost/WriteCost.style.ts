4fc4f2bc458f38b4693cc62125449b28c517ee10
P1su <pissssssu@naver.com>
2024-10-17 13:41:33 +0900

    feat: 지출 방법 선택 화면 구현

--- src/pages/WriteCost/WriteCost.style.ts
+++ src/pages/WriteCost/WriteCost.style.ts
@@ -3,7 +3,17 @@ import styled from "styled-components";
 export const WriteCostWrapper = styled.div`
 ${({ theme: { mixin } }) => mixin.flexCenter({ })};
 ${({ theme }) => theme.fonts.m_30_500};
-    color: ${({ theme }) => theme.colors.gray[700]};
-    width: 100%;
-    height: 100vh;
+	color: ${({ theme }) => theme.colors.gray[700]};
+	width: 100%;
+	padding: 6.4rem 1.6rem;
+`;
+export const SelectBox = styled.div`
+	${({ theme: { mixin } }) => mixin.flexBox({})};
+	padding-top: 4.4rem;
+	gap: 2.4rem;
+`;
+export const ItemBox = styled.div`
+	${({ theme: { mixin } }) => mixin.flexCenter({})};
+	${({ theme }) => theme.fonts.sb_16_600};
+	gap: 1.2rem;
 `;
--- src/pages/WriteCost/WriteCost.tsx
+++ src/pages/WriteCost/WriteCost.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import * as S from './WriteCost.style';
+import Title from '../../components/common/Title/Title';
+import { IcWriteAi, IcWriteSelf } from '../../assets/svg';
 
 const WriteCost = () => {
-    return (
-        <S.WriteCostWrapper>지출내용 기록 페이지 입니다.</S.WriteCostWrapper>
-    )
-}
+	return (
+		<S.WriteCostWrapper>
+			<Title>
+				🤖 <br />
+				지출 등록 방식을 <br />
+				선택해주세요
+			</Title>
+			<S.SelectBox>
+				<S.ItemBox>
+					<IcWriteAi />
+					AI 영수증
+				</S.ItemBox>
+				<S.ItemBox>
+					<IcWriteSelf />
+					직접 작성
+				</S.ItemBox>
+			</S.SelectBox>
+		</S.WriteCostWrapper>
+	);
+};
 
 export default WriteCost;
