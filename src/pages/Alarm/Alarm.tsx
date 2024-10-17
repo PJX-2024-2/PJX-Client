28b234e78ca731fb8f365f476ed47b0edeee9fc3
P1su <pissssssu@naver.com>
2024-10-17 11:04:46 +0900

    feat: Title 컴포넌트 배치 및 알림 없음 이미지 파일 추가

--- src/pages/Alarm/Alarm.tsx
+++ src/pages/Alarm/Alarm.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import * as S from './Alarm.style';
+import Title from '../../components/common/Title/Title';
 
 const Alarm = () => {
-    return (
-        <S.AlarmWrapper>알람페이지 입니다.</S.AlarmWrapper>
-    )
-}
+
+  return (
+    <S.AlarmWrapper>
+      <Title isLarge={false}>알림</Title>
+      <S.AlarmBox>
+      </S.AlarmBox>
+    </S.AlarmWrapper>
+  );
+};
 
 export default Alarm;
