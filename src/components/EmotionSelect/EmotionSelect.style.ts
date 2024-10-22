61325f21833e3b2942c10269f3450724ad2b645d
Wang HoEun <hoeunwang@gmail.com>
2024-10-22 13:16:17 +0900

    feat: buyCard padding생성 및 feed padding modal을 위해 지우기. modal 클릭 시 나타나도록 설정

--- src/components/EmotionSelect/EmotionSelect.style.ts
+++ src/components/EmotionSelect/EmotionSelect.style.ts
@@ -1,4 +1,18 @@
 import styled from 'styled-components';
 
 export const EmotionSelectWrapper = styled.div`
+width: 100%;
+background-color: none;
+`;
+
+export const EmotionModal = styled.div`
+position: absolute;
+width: 37.5rem;
+height: 16rem;
+bottom:0;
+left:0;
+background-color: white;
+border-radius: 1.4rem 1.4rem 0 0;
+border: 1px solid black;
+z-index: 4;
 `;
--- src/components/EmotionSelect/EmotionSelect.tsx
+++ src/components/EmotionSelect/EmotionSelect.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './EmotionSelect.style';
 import { IcPlusRound } from '../../assets/svg';
 
 const EmotionSelect = () => {
+    const [openModal,setOpenModal] = useState(false);
     return (
         <S.EmotionSelectWrapper>
-            <IcPlusRound style={{width: '3rem',height: '3rem'}}/>
+            <IcPlusRound style={{width: '3rem',height: '3rem'}} onClick={()=>setOpenModal(!openModal)}/>
+                {openModal? <S.EmotionModal>짜잔</S.EmotionModal>:null}
         </S.EmotionSelectWrapper>
     )
 }
