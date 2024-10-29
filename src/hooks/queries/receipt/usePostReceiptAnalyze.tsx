5d874ca4efbc49b9fd1e03802dfc9e9c87c3fee6
Wang HoEun <hoeunwang@gmail.com>
2024-10-29 19:42:06 +0900

    feat: ai post api 연결 query 코드 작성

--- src/hooks/queries/receipt/usePostReceiptAnalyze.tsx
+++ src/hooks/queries/receipt/usePostReceiptAnalyze.tsx
@@ -1,33 +1,28 @@
-// ai api 파일 생성
-import {useMutation, useQueryClient} from '@tanstack/react-query';
-import {aiPost} from '../../../apis/apiInstance';
-import {AxiosResponse} from 'axios';
+import { useMutation } from '@tanstack/react-query';
+import { aiPost } from '../../../apis/apiInstance';
+import { AxiosResponse } from 'axios';
 import { ReceiptImage } from '../../../types/receipt';
 
-export const POST_RECEIPT_QUERY_KEY: string[] = ['receiptData'];
+export const POST_RECEIPT_QUERY_KEY = ['receiptData'];
 
 export const postReceiptImage = async (body: ReceiptImage) => {
-    const formData = new FormData();
-    formData.append('files',body.file);
-    const response: AxiosResponse = await aiPost(`/receipt/analyze`,formData);
-    
-    return response.data;
+  const formData = new FormData();
+  formData.append('files', body.file); 
+
+  const response: AxiosResponse = await aiPost(`/receipt/analyze`, formData);
+  return response.data;
 };
 
-export const usePostReceiptAnalyze = ({body}:{body:ReceiptImage}) => {
-    const queryClient = useQueryClient();
-
-    const mutation = useMutation({
-        mutationFn : postReceiptImage,
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: [POST_RECEIPT_QUERY_KEY,body]});
-        },
-        onError: () => {
-            console.log('ERROR');
-        },
-    });
-    return {mutation};
-}
+export const usePostReceiptAnalyze = () => {
+  return useMutation({
+    mutationFn: postReceiptImage,
+    onSuccess: (data) => {
+      console.log('영수증 이미지 post 성공', data);
+    },
+    onError: () => {
+      console.log('ERROR');
+    },
+  });
+};
 
 export default usePostReceiptAnalyze;
-
