a9972cdccb4253caae38ac3f7ac3fd3cca9be58a
P1su <pissssssu@naver.com>
2024-11-01 20:08:36 +0900

    feat: 카카오 로그인 토큰 요청 코드 작성

--- src/components/LoginCallback/LoginCallback.tsx
+++ src/components/LoginCallback/LoginCallback.tsx
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react';
 import Loading from '../../pages/Loading/Loading';
 import axios from 'axios';
-import { REDIRECT_URI, REST_API_KEY } from '../../utils/login';
+import { REDIRECT_URI, REST_API_KEY, SERVER_BASE_URL } from '../../utils/login';
 
 const LoginCallback = () => {
   const code = new URL(document.location.toString()).searchParams.get('code');
   console.log(code);
-  const POST_URL = `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`;
+  console.log(SERVER_BASE_URL)
+  const getAccsessToken = async () => {
+    const response = await axios.get(`${SERVER_BASE_URL}/api/kakao/callbck`, {
+    params: {
+      code: code,
+    },
+    });
+    console.log(response);
+  }
+  /*const POST_URL = `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`;
 
   const postCode = async () => {
     const response = await axios.post(`https://kauth.kakao.com/oauth/token&`,
@@ -19,10 +28,11 @@ const LoginCallback = () => {
     );
     console.log(response);
   };
-
+*/
   useEffect(() => {
-    postCode();
+    getAccsessToken();
   })
+  
   return(
     <Loading />
   );
