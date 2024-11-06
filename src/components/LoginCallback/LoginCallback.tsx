20cbdef7bf089b6261732014da68beeaa4fabea9
P1su <pissssssu@naver.com>
2024-11-07 00:16:30 +0900

    feat: 로그인이 성공할 경우 home 으로 이동하는 이벤트 설정

--- src/components/LoginCallback/LoginCallback.tsx
+++ src/components/LoginCallback/LoginCallback.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect } from 'react';
 import Loading from '../../pages/Loading/Loading';
 import usePostAccessCode from '../../hooks/queries/login/usePostAccessCode';
+import { useNavigate } from 'react-router-dom';
 
 const LoginCallback = () => {
   const code = new URL(document.location.toString()).searchParams.get('code');
   const { mutate: postCode }  = usePostAccessCode();
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate('/home');
+  };
 
   useEffect(() => {
     if (code) {
       const body = {'code':code};
-      postCode(body);
+      postCode(body, {
+        onSuccess: () => {
+          handleNavigate();
+        },
+      });
     } else {
       console.error('Code parameter is missing from the URL.');
     }
