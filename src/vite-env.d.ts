578b37da8a0b69a12389c77e748bf36541caed6c
P1su <pissssssu@naver.com>
2024-11-11 16:55:52 +0900

    code review: 환경변수 설정 중 불필요한 코드 삭제

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_REST_API_KEY: string;
-  readonly VITE_LOCAL_REDIRECT_URI: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env, // 필요한 경우 환경 변수를 사용
-  },
 })
