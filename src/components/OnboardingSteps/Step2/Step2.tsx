40a32188b6a899e237a6076fc725a4d4a48dcd42
P1su <pissssssu@naver.com>
2024-11-20 14:34:21 +0900

    style: 사용하지 않는 코드 주석 처리

--- src/components/OnboardingSteps/Step2/Step2.tsx
+++ src/components/OnboardingSteps/Step2/Step2.tsx
@@ -6,11 +6,11 @@ import * as S from './Step2.style';
 import { ChangeEvent, useState } from 'react';
 import React from 'react';
 import useGetNickname from '../../../hooks/queries/onboarding/useGetNickname';
-import usePostNickname from '../../../hooks/queries/onboarding/usePostNickname';
+//import usePostNickname from '../../../hooks/queries/onboarding/usePostNickname';
 
 function Step2 ({ onNext }: StepProps){
   const [value, setValue] = useState<string>('');
-  const { mutate: postNickname } = usePostNickname();
+  //const { mutate: postNickname } = usePostNickname();
   const { isError } = useGetNickname(value);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
