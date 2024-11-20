ccd280c981c6dbf6839923c62ea5d835ca8e0e01
Wang HoEun <hoeunwang@gmail.com>
2024-11-20 11:49:28 +0900

    fix: api 명세서에 맞게 수정 및 한달 지출 내용 코드 작성

--- src/components/MonthlyCost/MonthlyCost.tsx
+++ src/components/MonthlyCost/MonthlyCost.tsx
@@ -2,10 +2,11 @@ import { IcPen } from '../../assets/svg';
 import React, { useState }  from "react";
 import * as S from './MonthlyCost.style';
 import { useNavigate } from 'react-router-dom';
-//import useGetPaid from '../../hooks/queries/home/useGetPaid';
+//import usePostPaid from '../../hooks/queries/home/usePostPaid';
 const MonthlyCost = () => {
     const navigate = useNavigate();
-    /*const userPaid = useGetPaid();*/
+    //const userPaid = usePostPaid();
+    //const paidResponse = userPaid.mutate("2024-11");
     const [currentAmount, setCurrentAmount] = useState(0);
     const maxGoal = parseInt(localStorage.getItem('MONTHLY_GOAL') || '0', 10);
     const handleAmount = () => {
@@ -15,8 +16,8 @@ const MonthlyCost = () => {
         <>
         <S.IcPenWrapper onClick={()=>navigate('/monthly-goal')}><IcPen width={'2.2rem'} height={'2.2rem'}/></S.IcPenWrapper>
             <S.MonthlyPaidWrapper>
-                <p><span>닉네임</span> 이번달 지출</p>
-                {currentAmount}원
+                <p ><span>닉네임</span> 이번달 지출</p>
+                원
             </S.MonthlyPaidWrapper>
             <S.MonthlyGoalWrapper>
                 <p>목표 지출 <span> {maxGoal}만원</span></p>
