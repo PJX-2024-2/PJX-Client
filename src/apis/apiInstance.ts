e4cf48b9789782f1eee3f8939c17c126bcf071ba
Wang HoEun <hoeunwang@gmail.com>
2024-11-20 14:19:27 +0900

    fix: instance 원래대로 되돌려두기

--- src/apis/apiInstance.ts
+++ src/apis/apiInstance.ts
@@ -26,7 +26,7 @@ export const serverInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_SERVER_BASE_URL,
     withCredentials: true,
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIzNzU0MDExMzgyIiwiaWF0IjoxNzMxOTQwNzMwLCJleHAiOjI0ODg4MDQ3MzB9.mZ-UYSbnlmaoiYpNlfbh3yQ6XIo7X5-kkZuRo5Oajysr1sF_sjnz3xIV_zQmiSt_mIhr3ovDOZVCChvjFbXJAA`,
+      Authorization: `Bearer ${getAccessTokenLocalStorage()}`,
     },
   });
 
