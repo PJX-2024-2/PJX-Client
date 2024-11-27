0db639623bd36ee0dc0d762e86d4726affd01c82
P1su <pissssssu@naver.com>
2024-11-27 22:35:42 +0900

    feat: aiInstance 헤더 수정

--- src/apis/apiInstance.ts
+++ src/apis/apiInstance.ts
@@ -26,14 +26,16 @@ export const serverInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_SERVER_BASE_URL,
     withCredentials: true,
     headers: {
-      //Authorization: `Bearer ${getAccessTokenLocalStorage()}`,
-      Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIzNzU0MDExMzgyIiwiaWF0IjoxNzMxOTQwNzMwLCJleHAiOjI0ODg4MDQ3MzB9.mZ-UYSbnlmaoiYpNlfbh3yQ6XIo7X5-kkZuRo5Oajysr1sF_sjnz3xIV_zQmiSt_mIhr3ovDOZVCChvjFbXJAA`
+      Authorization: `Bearer ${getAccessTokenLocalStorage()}`,
     },
   });
 
   export const aiInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_AI_BASE_URL,
     withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${getAccessTokenLocalStorage()}`,
+    },
   });
 
   function interceptorResponseFulfilled(res: AxiosResponse) {
--- src/hooks/queries/mypage/usePostSummary.tsx
+++ src/hooks/queries/mypage/usePostSummary.tsx
@@ -1,10 +1,11 @@
 export const SUMMARY_QUERY_KEY = ['summary'];
 import { useMutation } from '@tanstack/react-query';
-import axios, { AxiosResponse } from 'axios';
+import { AxiosResponse } from 'axios';
+import { aiPost } from '../../../apis/apiInstance';
 
 export const postSummary = async () => {
-  const body = { user_id: 8, };
-  const res: AxiosResponse = await axios.post('https://mytestserver.shop/api/v1/spending/analyze', body, {
+  const body = { user_id: localStorage.getItem('id'), };
+  const res: AxiosResponse = await aiPost('spending/analyze', body, {
     headers: {
       "Content-Type": 'application/json',
     },
