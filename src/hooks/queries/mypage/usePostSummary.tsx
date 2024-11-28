a396d04fdfba7669449459b9e492205df4e527ed
P1su <pissssssu@naver.com>
2024-11-28 14:21:41 +0900

    feat: summary 데이터에 따른 마이페이지 내용 변경

--- src/hooks/queries/mypage/usePostSummary.tsx
+++ src/hooks/queries/mypage/usePostSummary.tsx
@@ -3,7 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 import { aiPost } from '../../../apis/apiInstance';
 
-export const postSummary = async () => {
+export const postSummary = async ({}) => {
   const body = { user_id: localStorage.getItem('id'), };
   const res: AxiosResponse = await aiPost('spending/analyze', body, {
     headers: {
--- src/pages/Mypage/Mypage.tsx
+++ src/pages/Mypage/Mypage.tsx
@@ -1,70 +1,44 @@
 import React, { useEffect, useState } from "react";
 import * as S from './Mypage.style';
 import Title from './../../components/common/Title/Title';
-import BtnLarge from '../../components/common/Button/LargeButton/BtnLarge';
-import useGetProfile from '../../hooks/queries/mypage/useGetMypageInfo';
 import usePostSummary from '../../hooks/queries/mypage/usePostSummary';
-//import mockImage from '../../assets/profileMockImage.png';
-import { IcCheck, IcPen, IcRight, IcSearch } from '../../assets/svg';
+import { IcPen } from '../../assets/svg';
 import { useNavigate } from 'react-router-dom';
 import UserInfoContext, { UserInfoProvider } from '../../context/User/UserInfoContext';
 
 const Mypage = () => {
   const { userInfo } = React.useContext(UserInfoContext);
   const navigate = useNavigate();
-  //const  { data } = useGetProfile();
-  //console.log(data);
 
   const { mutate: postSummary } = usePostSummary();
-  const handleTemp = () => {
-    postSummary();
-  }/*
-  const options = [
-  /*  {
-      text: '내 지출 요약',
-      icon: <IcCheck fill='#000000' />,
-      onclick: () => {navigate('/')},
-    },
-    {
-      text: '내 또래 친구 목록',
-      icon: <IcSearch />,
-      onclick: () => {navigate('/mypage-friends')},
-    },
-  ];*/
 
   const [summaryValues, setSummaryValues] = useState({
-    days: 0,
-    costs: 0,
-    friends: 0,
+    amount: 0,
+    category: '',
+    rate: '',
   });
 
-  const summaries = [
-    {
-      label: 'days',
-      value: summaryValues.days,
-      text: '일 동안',
-    },
-    {
-      label: 'costs',
-      value: summaryValues.costs,
-      text: '원을 아꼈어요',
-    },
-    {
-      label: 'friends',
-      value: summaryValues.friends,
-      text: '명의 또래 친구가 생겼어요',
-    },
-  ];
-  
   const handleProfile = () => {
     navigate('/mypage-profile');
   };
-  const handleAiAnalyze = () => {
-    alert('ai분석');
-  };
+
+  useEffect(() => {
+    postSummary({}, {
+      onSuccess: (data) => {
+        console.log(data);
+        setSummaryValues(prevState => ({
+          ...prevState,
+          amount: data['총 지출 금액'],
+          category: data['주요 지출 카테고리'],
+          rate: data['월간 예산 대비 초과 또는 절약 금액'],
+        }));
+      },
+      
+    });
+  }, []);
+
   return (
     <UserInfoProvider>
-      <button onClick={() => handleTemp()}>버튼버튼</button>
       <S.MyPageWrapper>
         <Title isLarge={false}>마이페이지</Title>
         <S.ProfileBox>
@@ -77,28 +51,10 @@ const Mypage = () => {
           </S.NicknameBox>
         </S.ProfileBox>
         <S.SummaryBox>
-          {
-            summaries.map((summary) => (
-              <p key={summary.label}>
-                <S.SummarySpan $isDays={summary.label === 'days'}>
-                  {summary.value}
-                </S.SummarySpan>
-                {summary.text}
-              </p>
-            ))
-          }
+          <p>이번 달 총 <S.SummarySpan $isDays={true}>{summaryValues.amount}</S.SummarySpan> 원 지출</p>
+          <p>주요 지출 카테고리 <S.SummarySpan $isDays={false}>{summaryValues.category}</S.SummarySpan></p>
+          <p>월간 예상 대비 <S.SummarySpan $isDays={false}>{summaryValues.rate}</S.SummarySpan></p>
         </S.SummaryBox>
-        <S.NavigateField>
-          {/*
-            options.map((option) => (
-              <S.NavigateBox key={option.text} onClick={option.onclick}>
-                {option.icon}
-                <p>{option.text}</p>
-                <IcRight />
-              </S.NavigateBox>
-            ))*/
-          }
-        </S.NavigateField>
       </S.MyPageWrapper>
     </UserInfoProvider>
   );
